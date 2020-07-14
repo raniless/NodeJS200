533cebf4726647b4b0c6cbce8b70cbc197c56a5f
raniless <raniless@naver.com>
2020-07-14 21:12:31 +0900

    NodeJS 학습 5일차

---
+++ Part2_Begin/044_Function.js
@@ -0,0 +1,12 @@
+function printHello() {
+  console.log('hello');
+}
+
+printHello();
+
+//람다 표현식
+const printBye = () => {
+  console.log('bye');
+}
+
+printBye();
---
+++ Part2_Begin/045_ParamAndArg.js
@@ -0,0 +1,11 @@
+function printMessage(pMessage) {
+  console.log(pMessage);
+}
+
+printMessage('hello');
+
+const printMessage2 = (pMessage) => {
+  console.log(pMessage);
+}
+
+printMessage2('bye');
